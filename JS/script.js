c9b4eb5737be9c51c00940615e19730f4b94ac9f
Emmanuel Koech <koechemmanuel2002@gmail.com>
2022-10-13 20:16:17 +0300

    Loop through the showbutton

--- JS/script.js
+++ JS/script.js
@@ -8,10 +8,12 @@ let overlay = document.querySelector(`.overlay`)
 let closeButtonModal = document.querySelector(`.close-modal`)
 
 // Selected the show-modal element class
-
+// Used Query Selector all because it selects all elements with the class name
 let showButtonModal = document.querySelectorAll(`.show-modal`)
 console.log(showButtonModal)
 
+
+// Loop through the 
 for(let i = 0; i<showButtonModal.length; i++)
     showButtonModal[i].addEventListener(`click`,function(){
         console.log(`button clicked`)
