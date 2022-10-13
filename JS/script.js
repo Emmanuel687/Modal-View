fea83f74049d43cafff88a3ce8baa1ae48fc61c8
Emmanuel Koech <koechemmanuel2002@gmail.com>
2022-10-13 20:13:56 +0300

    Updated project selectors

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,14 @@
 // Selected the modal element class
 let modal = document.querySelector(`.modal`)
+
+// Selected the overlay element class
 let overlay = document.querySelector(`.overlay`)
+
+// Selected the close-modal element class
 let closeButtonModal = document.querySelector(`.close-modal`)
+
+// Selected the show-modal element class
+
 let showButtonModal = document.querySelectorAll(`.show-modal`)
 console.log(showButtonModal)
 
