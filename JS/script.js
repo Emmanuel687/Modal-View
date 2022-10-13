b0f10fb8030fb1be6124e9f5ff776db9c0f75617
Emmanuel Koech <koechemmanuel2002@gmail.com>
2022-10-13 20:11:22 +0300

    Selected the button from html

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,10 @@
-let modal = document.querySelector(`.show-modal`).addEventListener(`click`,()=>{
-
-})
-
+let modal = document.querySelector(`.modal`)
 let overlay = document.querySelector(`.overlay`)
-
 let closeButtonModal = document.querySelector(`.close-modal`)
+let showButtonModal = document.querySelectorAll(`.show-modal`)
+console.log(showButtonModal)
+
+for(let i = 0; i<showButtonModal.length; i++)
+    showButtonModal[i].addEventListener(`click`,function(){
+        console.log(`button clicked`)
+    })
